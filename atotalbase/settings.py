1cfd9854bf65b7bc0a208ac0feb2398d00f804a5
mk <twelvegold@gmail.com>
2022-05-09 13:08:13 +0900

    asset

--- atotalbase/settings.py
+++ atotalbase/settings.py
@@ -133,7 +133,7 @@ AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
-AWS_LOCATION = 'asset'
+AWS_LOCATION = 'static'
 AWS_DEFAULT_ACL = None
 STATIC_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME # STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
