8d86a32827683c0df0e7f2b94c6ee913ffadd634
mk <twelvegold@gmail.com>
2022-05-10 15:00:22 +0900

    maigo

--- atotalbase/settings.py
+++ atotalbase/settings.py
@@ -138,7 +138,7 @@ AWS_LOCATION = 'static'
 AWS_DEFAULT_ACL = None
 STATIC_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME  # STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-DEFAULT_FILE_STORAGE = 'atotalbase.storage_backends.MediaStorage'
+# DEFAULT_FILE_STORAGE = 'atotalbase.storage_backends.MediaStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
